0e8bc165045035df7bc6e86ff7d6066d06e74850
Electrojones <code@jonas-beck.de>
2019-11-28 21:56:50 +0100

    well, requests is unable to do the job

--- observation.py
+++ observation.py
@@ -1,15 +1,22 @@
-import numpy as np
+import requests
 import cv2
+import numpy as np
 
-cap = cv2.VideoCapture("http://192.168.2.103:8080/video")
+r=requests.get("http://192.168.2.103:8080/video", stream=True)
 
-while(True):
-
-    ret, frame = cap.read()
-    cv2.imshow('frame',frame)
-
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-cap.release()
-cv2.destroyAllWindows()
+if(r.status_code == 200):
+    bytes = bytes()
+    for chunk in r.iter_content():
+        bytes += chunk
+        print("lol")
+        a = bytes.find(b'\xff\xd8')
+        b = bytes.find(b'\xff\xd9')
+        if a != -1 and b != -1:
+            jpg = bytes[a:b+2]
+            bytes = bytes[b+2:]
+            i = cv2.imdecode(np.fromstring(jpg, dtype=np.uint8), cv2.IMREAD_COLOR)
+            cv2.imshow('i', i)
+            if cv2.waitKey(1) == 27:
+                exit(0)
+else:
+    print(r.status_code)
