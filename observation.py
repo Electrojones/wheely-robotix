7d756b6445f26e5e46f64fe91b51107e6aa6bf1b
Electrojones <code@jonas-beck.de>
2019-11-28 22:26:24 +0100

    FUCK ITS WORKING

--- observation.py
+++ observation.py
@@ -1,34 +1,20 @@
-# Using Android IP Webcam video .jpg stream (tested) in Python2 OpenCV3
-
-import urllib.request
-import cv2
+import requests
+from PIL import Image
+import io
 import numpy as np
-import time
+from matplotlib import pyplot as plt
 
-# Replace the URL with your own IPwebcam shot.jpg IP:port
-url='http://192.168.2.103:8080/shot.jpg'
+r=requests.get("http://192.168.2.103:8080/shot.jpg")
 
+print(r.content)
 
-while True:
-    # Use urllib to get the image from the IP camera
-    imgResp = urllib.request.urlopen(url)
-    
-    # Numpy to convert into a array
-    imgNp = np.array(bytearray(imgResp.read()),dtype=np.uint8)
-    
-    print(imgNp.shape)
+image = Image.open(io.BytesIO(r.content))
 
-    # Finally decode the array to OpenCV usable format ;) 
-    img = cv2.imdecode(imgNp,-1)
-	
-    
-	
-	# put the image on screen
-    cv2.imshow('IPWebcam',img)
+image.save("Image.jpg")
 
-    #To give the processor some less stress
-    #time.sleep(0.1) 
+image=np.asarray(image)
 
-    # Quit if q is pressed
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+print(image)
+
+plt.imshow(image)
+plt.show()
