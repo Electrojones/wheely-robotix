d27ee7bc1564c78810ad7fbcdfc79890537cf332
Electrojones <code@jonas-beck.de>
2019-11-29 23:01:29 +0100

    observation.py is ready

--- observation.py
+++ observation.py
@@ -3,18 +3,43 @@ from PIL import Image
 import io
 import numpy as np
 from matplotlib import pyplot as plt
+import cv2
 
-r=requests.get("http://192.168.2.103:8080/shot.jpg")
 
-print(r.content)
+#returns the position of the marker in the cameras field of view
+def get_marker_pos():
 
-image = Image.open(io.BytesIO(r.content))
+    #get the image, extract it, make an array out of it and convert the colorspace
+    r=requests.get("http://192.168.2.103:8080/shot.jpg")
+    image = Image.open(io.BytesIO(r.content))
+    image=np.asarray(image)
+    image=cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
 
-image.save("Image.jpg")
+    #boundaries to be considered blue
+    lower_blue = np.array([90,50,50])
+    upper_blue = np.array([150,255,255])
 
-image=np.asarray(image)
+    #search for blue areas
+    mask=cv2.inRange(image, lower_blue, upper_blue)
 
-print(image)
+    #locate blue pixels
+    x_coll=[]
+    y_coll=[]
 
-plt.imshow(image)
-plt.show()
+    for y in range(len(mask)):
+        for x in range(len(mask[0])):
+            if(mask[y, x]==255):
+                x_coll.append(x)
+                y_coll.append(y)
+
+    #calculate the center of blue in the image
+
+    y_cen=sum(y_coll)/len(y_coll)
+    x_cen=sum(x_coll)/len(x_coll)
+
+    return (x_cen,y_cen), mask
+
+if __name__ == "__main__":
+    pos, mask=get_marker_pos()
+    Image.fromarray(mask).save("Image.png")
+    print(pos)
---
+++ tests/observation_test.py
@@ -0,0 +1,40 @@
+import requests
+from PIL import Image
+import io
+import numpy as np
+from matplotlib import pyplot as plt
+import cv2
+
+r=requests.get("http://192.168.2.103:8080/shot.jpg")
+
+#print(r.content)
+
+image = Image.open(io.BytesIO(r.content))
+
+#image.save("Image.jpg")
+image=np.asarray(image)
+print(image)
+print("---")
+image=cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
+lower_blue = np.array([90,50,50])
+upper_blue = np.array([150,255,255])
+print(image)
+
+mask=cv2.inRange(image, lower_blue, upper_blue)
+
+
+Image.fromarray(mask).save("Image.png")
+
+x_coll=[]
+y_coll=[]
+
+for y in range(len(mask)):
+    for x in range(len(mask[0])):
+        if(mask[y, x]==255):
+            x_coll.append(x)
+            y_coll.append(y)
+
+print(len(image))
+
+print(sum(y_coll)/len(y_coll))
+print(sum(x_coll)/len(x_coll))
