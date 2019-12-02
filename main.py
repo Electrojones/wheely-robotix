f7c6841998fa3effa34a7081692b861303a2068f
Electrojones <code@jonas-beck.de>
2019-12-02 23:18:01 +0100

    added a folder

--- main.py
+++ main.py
@@ -3,22 +3,27 @@ import learning
 import servo
 
 #create the population
-pop=learning.create_pop(100, 100, 3, (3, 14))
+agents=learning.create_agents(100, 100, 3, (3, 14))
 
 #for explanation read the comment at the bottom
 try:
-    i=0
+    round=0
     while True:
-        print("round "+str(i)+" done")
+        print("round "+str(round)+" done")
         #evaluate + set score
 
         #select the best ones
-        pop=learning.select(pop, 0.2)
-        #breed
-        
-        #mutation
+        agents=learning.select(agents, 0.2)
 
-        #if condition
+        #breed
+        agents=learning.breed(agents, 100, 30)
+
+        #mutation
+        agents=learning.mutate(agents, 0.8)
+
+        #save it every 20 rounds
+        if i%20=0:
+            
             #save population
         
         i+=1
@@ -26,3 +31,5 @@ try:
 except KeyboardInterrupt:
     #to end the servos
     servo.end()
+
+servo.end()
