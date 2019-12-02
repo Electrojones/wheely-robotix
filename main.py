0d1a456ff23a90435d3f4a6beba8e992d1e61902
Electrojones <code@jonas-beck.de>
2019-12-02 23:32:40 +0100

    6 code lines still missing

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import observation
 import learning
 import servo
+from datetime import datetime
+import pickle
 
 #create the population
 agents=learning.create_agents(100, 100, 3, (3, 14))
@@ -12,6 +14,14 @@ try:
         print("round "+str(round)+" done")
         #evaluate + set score
 
+        #for agent in agents:
+            #do it 100 times
+                #set the servo angles
+                #wait a specific time
+            #pythagaros the distance
+            #set it to the agent
+
+
         #select the best ones
         agents=learning.select(agents, 0.2)
 
@@ -22,11 +32,14 @@ try:
         agents=learning.mutate(agents, 0.8)
 
         #save it every 20 rounds
-        if i%20=0:
-            
+        if round%20==0:
+            dateTimeObj = datetime.now()
+            timestamp = dateTimeObj.strftime("%d-%b-%Y_(%H:%M:%S.%f)")
+            writer=open(timestamp, 'wb')
+            pickle.dump(agents, writer)
             #save population
         
-        i+=1
+        round+=1
 
 except KeyboardInterrupt:
     #to end the servos
